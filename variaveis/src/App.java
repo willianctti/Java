996e914801a82f676e0adece120cb96123a1b1a3
willianctti <willianctti@gmail.com>
2023-07-25 19:21:46 -0300

    var

---
+++ variaveis/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Tipo nome
+        byte numeroPequeno = 125;
+        short numeroShow = 32760;
+        int numeroInt = 3298238;
+        long numeroEnorme = 29823984l;
+
+        // Variáveis do tipo ponto flutuante
+        float peso = 70.2f;
+        double pi = 3.1484389334894389;
+
+        // Variáveis do tipo cartactere
+        char letra = 'c';
+
+        // Varíavel booleano
+        boolean estouComFome = true;
+
+        // Variável string
+        String nome = "Willian";
+
+        System.out.println(numeroPequeno);
+        System.out.println(numeroShow);
+        System.out.println(numeroInt);
+        System.out.println(numeroEnorme);
+
+
+    }
+}
