e8b062c455882e8dcd0ccb11662a3b237cdedc54
willianctti <willianctti@gmail.com>
2023-08-03 17:20:52 -0300

    POO && CONSTRUCTOR

---
+++ construtores/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+
+   public class Circulo {
+    double diametro;
+    
+    public Circulo(double x) {
+        diametro = x;
+    }
+   }
+
+   public static void main(String[] args) {
+    // Circulo circ1 = new Circulo();
+
+   }
+
+
+
+
+
+   }
+
---
+++ poo1/src/App.java
@@ -0,0 +1,54 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    //    Carro van = new Carro();
+    //    van.marca = "Fiat";
+    //    van.modelo = "Ducato";
+    //    van.passageiros = 10;
+       
+    // }
+    }
+
+
+
+    // public class Lampada {
+    //     String modelo;
+    //     String tensao;
+    //     int potencia;
+    //     String cor;
+    //     String tipoLuz;
+
+
+
+    // }
+
+//     public class Lampada {
+//         String modelo;
+//         String tensao;
+//         int potencia;
+//         String cor;
+//         String tipoLuz;
+
+//         void exibirAutonomia() {
+//             System.out.println("A autonomia do carro é:" + tensao + tipoLuz);
+//         }
+//  }
+
+
+    public class Lampada {
+            String modelo;
+            String tensao;
+            int potencia;
+            String cor;
+            String tipoLuz;
+    
+            void exibirAutonomia() {
+                System.out.println("A autonomia do carro é:" + tensao + tipoLuz);
+            }
+     }
+
+
+
+
+}
+
+
